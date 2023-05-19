1b937900c6b4e2730ce324fd0247ce85f0a90fdd
iridmel0204 <132547192+iridmel0204@users.noreply.github.com>
2023-05-19 12:43:02 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    led.setBrightness(led.brightness() + receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(-25)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("POWER")
+})
+radio.onReceivedString(function (receivedString) {
+    let RECIVEDSTRING = 0
+    if (RECIVEDSTRING == 0) {
+        ENCENDIDO = !(ENCENDIDO)
+        led.setBrightness(125)
+        led.enable(ENCENDIDO)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(25)
+})
+let ENCENDIDO = false
+radio.setGroup(1)
